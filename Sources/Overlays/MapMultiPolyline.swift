1a89948916be5d48d3f9714eb88cf1ab32be7dda
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2022-04-25 23:36:37 +0200

    Remove fillColor from MapMultiPolyline

--- Sources/Overlays/MapMultiPolyline.swift
+++ Sources/Overlays/MapMultiPolyline.swift
@@ -16,47 +16,37 @@ public struct MapMultiPolyline: MapOverlay {
 
     public let overlay: MKOverlay
 
-    private let fillColor: Color?
-    private let nativeFillColor: NativeColor?
     private let lineWidth: CGFloat?
     private let strokeColor: Color?
     private let nativeStrokeColor: NativeColor?
 
     // MARK: Initialization
 
-    public init(polylines: [MKPolyline], fillColor: NativeColor? = nil, lineWidth: CGFloat? = nil, strokeColor: NativeColor? = nil) {
+    public init(polylines: [MKPolyline], lineWidth: CGFloat? = nil, strokeColor: NativeColor? = nil) {
         self.overlay = MKMultiPolyline(polylines)
-        self.fillColor = nil
-        self.nativeFillColor = fillColor
         self.lineWidth = lineWidth
         self.strokeColor = nil
         self.nativeStrokeColor = strokeColor
     }
 
-    public init(multiPolyline: MKMultiPolyline, fillColor: NativeColor? = nil, lineWidth: CGFloat? = nil, strokeColor: NativeColor? = nil) {
+    public init(multiPolyline: MKMultiPolyline, lineWidth: CGFloat? = nil, strokeColor: NativeColor? = nil) {
         self.overlay = multiPolyline
-        self.fillColor = nil
-        self.nativeFillColor = fillColor
         self.lineWidth = lineWidth
         self.strokeColor = nil
         self.nativeStrokeColor = strokeColor
     }
 
     @available(iOS 14, macOS 11, tvOS 14, *)
-    public init(polylines: [MKPolyline], fillColor: Color?, lineWidth: CGFloat? = nil, strokeColor: Color?) {
+    public init(polylines: [MKPolyline], lineWidth: CGFloat? = nil, strokeColor: Color?) {
         self.overlay = MKMultiPolyline(polylines)
-        self.fillColor = fillColor
-        self.nativeFillColor = nil
         self.lineWidth = lineWidth
         self.strokeColor = strokeColor
         self.nativeStrokeColor = nil
     }
 
     @available(iOS 14, macOS 11, tvOS 14, *)
-    public init(multiPolyline: MKMultiPolyline, fillColor: Color?, lineWidth: CGFloat? = nil, strokeColor: Color?) {
+    public init(multiPolyline: MKMultiPolyline, lineWidth: CGFloat? = nil, strokeColor: Color?) {
         self.overlay = multiPolyline
-        self.fillColor = fillColor
-        self.nativeFillColor = nil
         self.lineWidth = lineWidth
         self.strokeColor = strokeColor
         self.nativeStrokeColor = nil
@@ -69,11 +59,6 @@ public struct MapMultiPolyline: MapOverlay {
             .map { MKMultiPolylineRenderer(multiPolyline: $0) }
             ?? MKMultiPolylineRenderer(overlay: overlay)
 
-        if let fillColor = fillColor, #available(iOS 14, macOS 11, tvOS 14, *) {
-            renderer.fillColor = .init(fillColor)
-        } else if let fillColor = nativeFillColor {
-            renderer.fillColor = fillColor
-        }
         if let lineWidth = lineWidth {
             renderer.lineWidth = lineWidth
         }
