7ea2440d83054878c801e16ef960ffec41062640
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2023-02-09 18:59:38 +0100

    Improve availability precompiler directives

--- Sources/Configuration/MapInformationVisibility.swift
+++ Sources/Configuration/MapInformationVisibility.swift
@@ -9,33 +9,45 @@ public struct MapInformationVisibility: OptionSet {
 
     // MARK: Static Properties
 
-    #if !os(watchOS)
+    @available(watchOS, unavailable)
     public static let buildings = MapInformationVisibility(rawValue: 1 << 0)
-    #endif
 
-    #if !os(tvOS) && !os(watchOS)
+    @available(tvOS, unavailable)
+    @available(watchOS, unavailable)
     public static let compass = MapInformationVisibility(rawValue: 1 << 1)
-    #endif
 
-    #if os(macOS) || targetEnvironment(macCatalyst)
-    @available(macOS 11, macCatalyst 14, *)
+    #if targetEnvironment(macCatalyst)
+    @available(macCatalyst 14, *)
+    public static let pitchControl = MapInformationVisibility(rawValue: 1 << 2)
+    #else
+    @available(iOS, unavailable)
+    @available(macOS 11, *)
+    @available(tvOS, unavailable)
+    @available(watchOS, unavailable)
     public static let pitchControl = MapInformationVisibility(rawValue: 1 << 2)
     #endif
 
-    #if !os(watchOS)
+    @available(watchOS, unavailable)
     public static let scale = MapInformationVisibility(rawValue: 1 << 3)
-    public static let traffic = MapInformationVisibility(rawValue: 1 << 4)
-    #endif
 
-    #if os(watchOS)
+    @available(watchOS, unavailable)
+    public static let traffic = MapInformationVisibility(rawValue: 1 << 4)
+
+    @available(iOS, unavailable)
+    @available(macOS, unavailable)
+    @available(tvOS, unavailable)
     @available(watchOS 6.1, *)
     public static let userHeading = MapInformationVisibility(rawValue: 1 << 5)
-    #endif
 
     @available(watchOS 6.1, *)
     public static let userLocation = MapInformationVisibility(rawValue: 1 << 6)
 
-    #if os(macOS) || targetEnvironment(macCatalyst)
+    #if targetEnvironment(macCatalyst)
+    public static let zoomControls = MapInformationVisibility(rawValue: 1 << 7)
+    #else
+    @available(iOS, unavailable)
+    @available(tvOS, unavailable)
+    @available(watchOS, unavailable)
     public static let zoomControls = MapInformationVisibility(rawValue: 1 << 7)
     #endif
 
